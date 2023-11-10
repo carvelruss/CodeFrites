9031df4be49954ff29b64e81e8590fbe91212369
Carvel Russ <work.carvelruss@gmail.com>
2023-11-10 20:18:07 +0800

    Update the dynamic-head

--- assets/vendor/dynamic-head/dynamic-head.js
+++ assets/vendor/dynamic-head/dynamic-head.js
@@ -2,11 +2,6 @@
 
 // Function to dynamically create head elements for tracking codes
 function createDynamicHead() {
-  // Facebook Pixel
-  var facebookPixelScript = document.createElement("script");
-  facebookPixelScript.src = "your_facebook_pixel_script_url";
-  facebookPixelScript.async = true;
-
   // Google Analytics gtag.js
   var gtagScript = document.createElement("script");
   gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-DMGJFG28FB";
@@ -20,16 +15,9 @@ function createDynamicHead() {
     gtag('config', 'G-DMGJFG28FB');
   `;
 
-  // Google Tag Manager
-  var gtmScript = document.createElement("script");
-  gtmScript.src = "your_google_tag_manager_script_url";
-  gtmScript.async = true;
-
   // Append elements to head
-  document.head.appendChild(facebookPixelScript);
   document.head.appendChild(gtagScript);
   document.head.appendChild(gtagConfigScript);
-  document.head.appendChild(gtmScript);
 }
 
 // Call the function to create dynamic head elements
