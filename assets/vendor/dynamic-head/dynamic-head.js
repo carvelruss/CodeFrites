31727cf0a17f0147dee69863121843428e660391
Carvel Russ <work.carvelruss@gmail.com>
2023-11-15 17:55:19 +0800

    Removed dynamic-header

--- assets/vendor/dynamic-head/dynamic-head.js
+++ assets/vendor/dynamic-head/dynamic-head.js
@@ -1,21 +1,17 @@
-// Function to dynamically include head content
-function includeHeadContent() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/head/headContent.html", true);
+// function includeHeadContent() {
+//   var xhr = new XMLHttpRequest();
+//   xhr.open("GET", "/head/headContent.html", true);
 
-  xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
-      // Create a script element and set its content
-      var headContent = document.createElement("script");
-      headContent.innerHTML = xhr.responseText;
+//   xhr.onload = function () {
+//     if (xhr.status >= 200 && xhr.status < 300) {
+//       var headContent = document.createElement("script");
+//       headContent.innerHTML = xhr.responseText;
 
-      // Append element to head
-      document.head.appendChild(headContent);
-    }
-  };
+//       document.head.appendChild(headContent);
+//     }
+//   };
 
-  xhr.send();
-}
+//   xhr.send();
+// }
 
-// Call the function to include head content
-includeHeadContent();
+// includeHeadContent();
