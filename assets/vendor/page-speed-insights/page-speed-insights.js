c3f1fbe6a0a83d33cf110e7037f50f246bfa03a1
Carvel Russ <work.carvelruss@gmail.com>
2023-11-18 05:40:58 +0800

    Hide the Page SPeed Insights

--- assets/vendor/page-speed-insights/page-speed-insights.js
+++ assets/vendor/page-speed-insights/page-speed-insights.js
@@ -12,29 +12,35 @@ loadingIndicator.style.display = "block";
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    // Check if 'lighthouseResult' and 'categories' exist in the response
-    if (data && data.lighthouseResult && data.lighthouseResult.categories) {
-      const lighthouseData = data.lighthouseResult.categories;
-      const performanceScore = lighthouseData.performance.score * 100 || 0;
-      const accessibilityScore = lighthouseData.accessibility.score * 100 || 0;
-      const bestPracticesScore =
-        lighthouseData["best-practices"].score * 100 || 0;
-      const seoScore = lighthouseData.seo.score * 100 || 0;
+    try {
+      // Check if 'lighthouseResult' and 'categories' exist in the response
+      if (data && data.lighthouseResult && data.lighthouseResult.categories) {
+        const lighthouseData = data.lighthouseResult.categories;
+        const performanceScore = lighthouseData.performance.score * 100 || 0;
+        const accessibilityScore =
+          lighthouseData.accessibility.score * 100 || 0;
+        const bestPracticesScore =
+          lighthouseData["best-practices"].score * 100 || 0;
+        const seoScore = lighthouseData.seo.score * 100 || 0;
 
-      const scoresContainer = document.getElementById("scores-container");
+        const scoresContainer = document.getElementById("scores-container");
 
-      scoresContainer.innerHTML = `
-                        <h2>Performance: ${performanceScore}</h2>
-                        <h2>Accessibility: ${accessibilityScore}</h2>
-                        <h2>Best Practices: ${bestPracticesScore}</h2>
-                        <h2>SEO: ${seoScore}</h2>
-                    `;
-    } else {
-      throw new Error("Invalid response structure from PageSpeed Insights API");
+        scoresContainer.innerHTML = `
+                            <h2>Performance: ${performanceScore}</h2>
+                            <h2>Accessibility: ${accessibilityScore}</h2>
+                            <h2>Best Practices: ${bestPracticesScore}</h2>
+                            <h2>SEO: ${seoScore}</h2>
+                        `;
+      } else {
+        throw new Error("Categories not found in API response");
+      }
+    } catch (error) {
+      console.error("Error processing data:", error);
+      // Hide loading indicator in case of an error
+      loadingIndicator.style.display = "none";
     }
 
-    // Hide loading indicator once data is fetched
+    // Hide loading indicator once data is fetched (even in case of an error)
     loadingIndicator.style.display = "none";
   })
   .catch((error) => {
