c676dadf5bea15895a1bb8c7dacbeba20a237582
Carvel Russ <work.carvelruss@gmail.com>
2023-11-18 05:10:13 +0800

    added Loading Screen

--- assets/vendor/page-speed-insights/page-speed-insights.js
+++ assets/vendor/page-speed-insights/page-speed-insights.js
@@ -1,9 +1,13 @@
 const apiKey = "AIzaSyCvzguDuWIwhrbJ2ri7RE-HVHZhf5N9WKs";
-const urlToTest = "https://codefrites.com";
+const urlToTest = "https://codefrites.com"; // Replace with your URL
 
 const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(
   urlToTest
 )}&key=${apiKey}`;
+
+// Display loading indicator
+const loadingIndicator = document.getElementById("loading-indicator");
+loadingIndicator.style.display = "block";
 
 fetch(apiUrl)
   .then((response) => response.json())
@@ -19,12 +23,17 @@ fetch(apiUrl)
     const scoresContainer = document.getElementById("scores-container");
 
     scoresContainer.innerHTML = `
-                <h2>Performance: ${performanceScore}</h2>
-                <h2>Accessibility: ${accessibilityScore}</h2>
-                <h2>Best Practices: ${bestPracticesScore}</h2>
-                <h2>SEO: ${seoScore}</h2>
-            `;
+                    <h2>Performance: ${performanceScore}</h2>
+                    <h2>Accessibility: ${accessibilityScore}</h2>
+                    <h2>Best Practices: ${bestPracticesScore}</h2>
+                    <h2>SEO: ${seoScore}</h2>
+                `;
+
+    // Hide loading indicator once data is fetched
+    loadingIndicator.style.display = "none";
   })
   .catch((error) => {
     console.error("Error fetching data:", error);
+    // Hide loading indicator in case of an error
+    loadingIndicator.style.display = "none";
   });
