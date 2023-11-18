6109b7d19f3888ed4abec03f8f1ca5e81475ebc2
Carvel Russ <work.carvelruss@gmail.com>
2023-11-18 14:00:44 +0800

    Update the Page Speed Insight Script nad HTML

--- assets/vendor/page-speed-insights/page-speed-insights.js
+++ assets/vendor/page-speed-insights/page-speed-insights.js
@@ -1,50 +1,34 @@
 const apiKey = "AIzaSyCvzguDuWIwhrbJ2ri7RE-HVHZhf5N9WKs";
-const urlToTest = "https://codefrites.com"; // Replace with your URL
+const urlToTest = "https://codefrites.com";
 
 const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${encodeURIComponent(
   urlToTest
 )}&key=${apiKey}`;
 
-// Display loading indicator
-const loadingIndicator = document.getElementById("loading-indicator");
-loadingIndicator.style.display = "block";
-
 fetch(apiUrl)
   .then((response) => response.json())
   .then((data) => {
-    try {
-      // Check if 'lighthouseResult' and 'categories' exist in the response
-      if (data && data.lighthouseResult && data.lighthouseResult.categories) {
-        const lighthouseData = data.lighthouseResult.categories;
-        const performanceScore = lighthouseData.performance.score * 100 || 0;
-        const accessibilityScore =
-          lighthouseData.accessibility.score * 100 || 0;
-        const bestPracticesScore =
-          lighthouseData["best-practices"].score * 100 || 0;
-        const seoScore = lighthouseData.seo.score * 100 || 0;
+    const performanceScore =
+      data.lighthouseResult.categories.performance.score * 100 || 0;
+    const accessibilityScore =
+      data.lighthouseResult.categories.accessibility.score * 100 || 0;
+    const bestPracticesScore =
+      data.lighthouseResult.categories["best-practices"].score * 100 || 0;
+    const seoScore = data.lighthouseResult.categories.seo.score * 100 || 0;
 
-        const scoresContainer = document.getElementById("scores-container");
-
-        scoresContainer.innerHTML = `
-                            <h2>Performance: ${performanceScore}</h2>
-                            <h2>Accessibility: ${accessibilityScore}</h2>
-                            <h2>Best Practices: ${bestPracticesScore}</h2>
-                            <h2>SEO: ${seoScore}</h2>
-                        `;
-      } else {
-        throw new Error("Categories not found in API response");
-      }
-    } catch (error) {
-      console.error("Error processing data:", error);
-      // Hide loading indicator in case of an error
-      loadingIndicator.style.display = "none";
-    }
-
-    // Hide loading indicator once data is fetched (even in case of an error)
-    loadingIndicator.style.display = "none";
+    document.getElementById(
+      "performance"
+    ).innerHTML = `<p>Performance: ${performanceScore}</p>`;
+    document.getElementById(
+      "accessibility"
+    ).innerHTML = `<p>Accessibility: ${accessibilityScore}</p>`;
+    document.getElementById(
+      "bestPractices"
+    ).innerHTML = `<p>Best Practices: ${bestPracticesScore}</p>`;
+    document.getElementById("seo").innerHTML = `<p>SEO: ${seoScore}</p>`;
   })
   .catch((error) => {
     console.error("Error fetching data:", error);
-    // Hide loading indicator in case of an error
-    loadingIndicator.style.display = "none";
+    document.getElementById("pageSpeedInsights").innerHTML =
+      "<p>Error fetching PageSpeed Insights data</p>";
   });
